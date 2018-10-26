2d35746560be987e3886b20bef8eb070e7f1e434
asapjack <jack.lampinen@maine.edu>
2018-10-26 18:41:03 -0400

    Init Commit

---
+++ MAGGAPP/DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "mysql.magapp.nmdprojects.net";
+
+//Define your database name here.
+$DatabaseName = "magapp";
+
+//Define your database username here.
+$HostUser = "magappdbadmin";
+
+//Define your database password here.
+$HostPass = "JackElizaTate";
+
+?>
